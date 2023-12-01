eb4b1d806600ebf33bb6e0e93b93cbaa42d5a9e9
alachhman <33943685+alachhman@users.noreply.github.com>
2023-11-30 23:53:30 -0500

    redesign initial commit

--- src/App.js
+++ src/App.js
@@ -1,88 +1,40 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import './box.css';
 
-const prices = {
-    rice: 6,
-    gyro: 4,
-    drink: 1
+let boxStyle = {
+    marginLeft: "19px",
+    marginTop: "19px",
+    display: "flex",
+    justifyContent: "left",
+    alignItems: "center",
+    transition: "all 0.3s ease",
+    overflow: "hidden"
 }
 
 function App() {
-    let [riceOut, setRiceOut] = React.useState(null);
-    let [gyroOut, setGyroOut] = React.useState(null);
-    let [meatPref, setMeatPref] = React.useState("Lamb");
-    let [currency, setCurrency] = React.useState("USD");
+    const [animDone, setAnimDone] = useState(false)
+    const [questionText, setQuestionText] = useState("How Much Halal Can I Afford?")
 
     useEffect(() => {
-        document.title = "How Much Halal Can I Afford?"
+        setTimeout(() => {
+            setAnimDone(true)
+        }, 3000)
     }, []);
 
-    const toggleMeat = () => {
-        console.log("Test")
-        setMeatPref(value => (value === "Lamb") ? "Chicken" : "Lamb")
-    }
-    const updateOutput = (x) => {
-        let input = Math.floor(x.target.value);
-        let data = {
-            prefRice: {
-                rice: Math.floor(input / prices.rice),
-                gyro: Math.floor(input % prices.rice / prices.gyro),
-                drink: (input < prices.gyro) ? input / prices.drink : Math.floor(input % prices.rice % prices.gyro / prices.drink)
-            },
-            prefGyro: {
-                gyro: Math.floor(input / prices.gyro),
-                drink: (input < prices.gyro) ? input / prices.drink : Math.floor(input % prices.gyro / prices.drink)
-            }
-        }
-        setRiceOut(outputRender(data.prefRice));
-        setGyroOut(outputRender(data.prefGyro));
-    }
-    const outputRender = (data) => {
-        return (
-            (data.rice <= 0 && data.gyro <= 0 && data.drink <= 0) ? null :
-                <p>
-                    {
-                        data.rice > 0 ? <div>
-                            {data.rice + " " + meatPref + " Over Rice"}
-                        </div> : ""
-                    }
-
-                    {
-                        data.gyro > 0 ? <div>
-                            {data.gyro + " " + meatPref + " Gyro"}
-                        </div> : ""
-                    }
-
-                    {
-                        data.drink > 0 ? <div>
-                            {((data.rice > 0 || data.gyro > 0) ? "and " : "") + data.drink + " drinks"}
-                        </div> : ""
-                    }
-                </p>
-        );
-    }
-
     return (
-        <div className="App">
-            <header className="App-header">
-                <div className="App-logo" onClick={() => {
-                    toggleMeat()
-                }}>🥙
+        <div className="container">
+            <div className="box">
+                <div className="title" style={boxStyle}>
+                    🥙
+                    {
+                        animDone &&
+                        <h3 className="titleText" style={{marginLeft: "16px"}}>
+                            How Much Halal Can I Afford?
+                        </h3>
+                    }
                 </div>
-                <p>
-                    $
-                    <input className={"Input"} type="number" name="name" max={1000000} min={0} placeholder={6}
-                           onChange={(x) => {
-                               updateOutput(x)
-                           }}/>
-                    will get you:
-                </p>
-                <p>
-                    {"Nothing." && riceOut}
-                    {(riceOut || gyroOut) ? "---- OR ----" : null}
-                    {"Nothing." && gyroOut}
-                </p>
-            </header>
+            </div>
         </div>
     );
 }
