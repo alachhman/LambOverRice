4faf7c28653da10007296c467b4cf99b82a9af24
Anthony Lachhman <antnee@Anthonys-MacBook-Pro.local>
2024-01-08 01:14:24 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import './box.css';
 import Questions from "./questions";
--- src/components/confirmationQuestion.js
+++ src/components/confirmationQuestion.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
-const ConfirmationQuestion = ({results}) => {
+const ConfirmationQuestion = (props) => {
+    const results = props.results
     return (
         <div className={"resultsContainer"}>
             <div>You have ${results.budget} to spend on</div>
             <div>
                 {
                     results.carb === "Either"
-                        ? results.meat.toLocaleLowerCase() + " over rice, or in a gyro,"
-                        : results.meat.toLocaleLowerCase() + (results.carb === "Rice") ? " over rice," : " gyro,"
+                        ? results.meat + " over rice, or in a gyro,"
+                        : results.meat + (results.carb === "Rice") ? " over rice," : " gyro,"
                 }
             </div>
             <div>
-                and you {(results.bev === "Yes" ? "" : " don't")} want a beverage.
+                and you {(results.bev === "Yes" ? "" : " don't")} want a beverage?
             </div>
         </div>
     );
